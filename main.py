57706a23c6460784cab603dc00be9ebac2d35341
Bhupesh-V <varshneybhupesh@gmail.com>
2022-06-23 22:56:57 +0530

    🐛 fix bugs + solution complete

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 from parkinglot.parkinglot import ParkingLot
 
+
 def readInstructions():
     with open("input.txt", "r") as file:
         for line in file:
             instruction = line.rstrip().split(" ")
 
             if instruction[0] == "Create_parking_lot":
-                print("Create lot")
-                p = ParkingLot()
-                print(p)
+                p = ParkingLot(int(instruction[1]))
+                print(f"Created parking of {instruction[1]} slots")
+
             elif instruction[0] == "Park":
-                print("Park car with number", instruction[1])
+                slot = p.park_vehicle(instruction[1], int(instruction[3]))
+                print(
+                    f'Car with vehicle registration number "{instruction[1]}" has been parked at slot number {slot}'
+                )
+
             elif instruction[0] == "Slot_numbers_for_driver_of_age":
-                print("get slot numbers")
+                slot_nos = p.get_slot_numbers(int(instruction[1]))
+                print(",".join(str(slot) for slot in slot_nos))
+
             elif instruction[0] == "Slot_number_for_car_with_number":
-                print("get slot number for car")
+                print(p.get_slot(instruction[1]))
+
             elif instruction[0] == "Leave":
-                print("vacate slot")
+                slot_data = p.vacate_slot(int(instruction[1]))
+                print(
+                    f"Slot number {instruction[1]} vacated, the car with vehicle registration number {slot_data.vehicle_registration_number} left the space, the driver of the car was of age {slot_data.age_of_driver}"
+                )
+
             elif instruction[0] == "Vehicle_registration_number_for_driver_of_age":
-                print("get reg number for driver age,", instruction[1])
+                vehicles = p.get_vehicle_registration(int(instruction[1]))
+                print(",".join(vehicles))
+
             else:
                 print("Unrecognised instruction, skipping")
+
 
 if __name__ == "__main__":
     readInstructions()
--- parkinglot/parkinglot.py
+++ parkinglot/parkinglot.py
@@ -1,55 +1,79 @@
-class Slot(dict):
+from typing import List
+from collections import OrderedDict
+
+
+class Slot:
     """Slot data"""
 
-    vehicle_registration_number: str
-    age_of_driver: int
+    def __init__(self, vehicle_registration_number: str, age_of_driver: int):
+        self.vehicle_registration_number = vehicle_registration_number
+        self.age_of_driver = age_of_driver
+
 
 class ParkingLot:
-
     def __init__(self, slots_count):
         self.slots_count = slots_count
-        # slots are assigned in order from 0 to n -1
-        self.slots = dict.fromkeys([n for n in range(self.slots_count)])
+        self.is_lot_full = False
+        # slots are assigned in order from 1 to N
+        self.slots = OrderedDict.fromkeys([n for n in range(1, self.slots_count + 1)])
 
-    def park_vehicle(vehicle_registration_number, driver_age):
+    def park_vehicle(self, vehicle_registration_number, driver_age) -> int:
         """A new vehicle enters the parking lot, create a new slot for them"""
-        if len(self.slots) < self.slots_count:
+        if not self.is_lot_full:
             available_slot = self.__get_next_slot()
-            self.slots[available_slot] = Slot(self.vehicle_registration_number, self.driver_age)
+            slot = Slot(
+                vehicle_registration_number=vehicle_registration_number,
+                age_of_driver=driver_age,
+            )
+            self.slots[available_slot] = slot
+            return available_slot
         else:
+            self.is_lot_full = True
             print("Request to park rejected, No empty slots at the moment")
 
-    def get_slot_numbers(driver_age: int) -> List[int]:
+    def get_slot_numbers(self, driver_age: int) -> List[int]:
         slots_with_driver_age = []
         for slot in self.slots:
-            if slot.driver_age == driver_age:
+            if (
+                self.slots[slot] is not None
+                and self.slots[slot].age_of_driver == driver_age
+            ):
                 slots_with_driver_age.append(slot)
         return slots_with_driver_age
 
-
-    def get_slot(vehicle_registration_number: str):
+    def get_slot(self, vehicle_registration_number: str):
         for slot in self.slots:
-            if slot.vehicle_registration_number == vehicle_registration_number:
+            if (
+                self.slots[slot].vehicle_registration_number
+                == vehicle_registration_number
+            ):
                 return slot
 
-    def get_vehicle_registration(driver_age: int) -> List[str]:
+    def get_vehicle_registration(self, driver_age: int) -> List[str]:
         vehicles = []
         for slot in self.slots:
-            if slot.driver_age == driver_age:
+            if (
+                self.slots[slot] is not None
+                and self.slots[slot].age_of_driver == driver_age
+            ):
                 vehicles.append(slot.vehicle_registration_number)
         return vehicles
 
-    def vacate_slot(slot_number: int):
+    def vacate_slot(self, slot_number: int) -> Slot:
         """A vehicle wants to leave the parking lot, vacate their slot"""
         if slot_number not in self.slots.keys():
             print("Invalid slot number")
+            return
         else:
+            deleted_data = self.slots[slot_number]
             # make it None (i.e available to book again)
             self.slots[slot_number] = None
+        return deleted_data
 
-    def __get_next_slot() -> int:
+    def __get_next_slot(self) -> int:
         """Get next available free slot"""
         for slot in self.slots:
             if self.slots[slot] is None:
                 # get the first available slot, assuming its nearest to entry
                 return slot
+        return None
